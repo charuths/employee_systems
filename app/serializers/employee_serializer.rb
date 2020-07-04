59db290034f2fe9b2e0a595b0b8079b8a4032424
Charuth Sudhakaran <charuthsudhakaran@gmail.com>
2020-07-04 20:45:20 +0530

    fix the serilizer

--- app/serializers/employee_serializer.rb
+++ app/serializers/employee_serializer.rb
@@ -1,4 +1,4 @@
-class EmployeeSerializer < ActiveModel::Serializer
+class EmployeeSerializer < ActiveModel::Serializers
   include FastJsonapi::ObjectSerializer
   attributes :name, :date_of_birth, :gender, :marital_status
 end
