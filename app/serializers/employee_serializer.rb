f4a3daad734e7f5102cd9e225218cb707f8149f7
Charuth Sudhakaran <charuthsudhakaran@gmail.com>
2020-07-04 20:53:18 +0530

    remove the lines from serilizer

--- app/serializers/employee_serializer.rb
+++ app/serializers/employee_serializer.rb
@@ -1,4 +1,4 @@
-class EmployeeSerializer < ActiveModel::Serializers
-  include FastJsonapi::ObjectSerializer
-  attributes :name, :date_of_birth, :gender, :marital_status
-end
+# class EmployeeSerializer < ActiveModel::Serializers
+#   include FastJsonapi::ObjectSerializer
+#   attributes :name, :date_of_birth, :gender, :marital_status
+# end
