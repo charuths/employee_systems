f9fc177daf9f6ace0ff6bbc8ad2304f83d95e780
charuths <charuthsudhakaran@gmail.com>
2021-05-04 12:45:29 +0530

    add sample data on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,20 @@ employee1 = Employee.create(name: 'employee1', date_of_birth: '2000-jun-30',gend
 employee2 = Employee.create(name: 'employee2', date_of_birth: '2000-jun-30',gender: 'female',marital_status: 'married')
 employee3 = Employee.create(name: 'employee3', date_of_birth: '2000-jun-30',gender: 'male',marital_status: 'others')
 employee4 = Employee.create(name: 'employee4', date_of_birth: '2000-jun-30',gender: 'female',marital_status: 'single')
+
+
+
+
+employe_address1 =  EmployeeAddress.create( adress_line_1: "xxxxx", address_line_2: "yyyyy", postal_code: "678001")
+employe_address2 =  EmployeeAddress.create( adress_line_1: "xxxxx", address_line_2: "yyyyy", postal_code: "678001")
+employe_address3 =  EmployeeAddress.create( adress_line_1: "xxxxx", address_line_2: "yyyyy", postal_code: "678001")
+employe_address4 =  EmployeeAddress.create( adress_line_1: "xxxxx", address_line_2: "yyyyy", postal_code: "678001")
+employe_address5 =  EmployeeAddress.create( adress_line_1: "xxxxx", address_line_2: "yyyyy", postal_code: "678001")
+employe_address6 =   EmployeeAddress.create( adress_line_1: "xxxxx", address_line_2: "yyyyy", postal_code: "678001")
+employe_address7 =  EmployeeAddress.create( adress_line_1: "xxxxx", address_line_2: "yyyyy", postal_code: "678001")
+employe_address8 =  EmployeeAddress.create( adress_line_1: "xxxxx", address_line_2: "yyyyy", postal_code: "678001")
+employe_address9 =  EmployeeAddress.create( adress_line_1: "xxxxx", address_line_2: "yyyyy", postal_code: "678001")
+employe_address11 =  EmployeeAddress.create( adress_line_1: "xxxxx", address_line_2: "yyyyy", postal_code: "678001")
+employe_address12 =  EmployeeAddress.create( adress_line_1: "xxxxx", address_line_2: "yyyyy", postal_code: "678001")
+employe_address13 =  EmployeeAddress.create( adress_line_1: "xxxxx", address_line_2: "yyyyy", postal_code: "678001")
+employe_address14 =  EmployeeAddress.create( adress_line_1: "xxxxx", address_line_2: "yyyyy", postal_code: "678001")
